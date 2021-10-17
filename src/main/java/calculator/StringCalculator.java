474299886537b7a6460314e0ea8cad12fc14e843
Akash-R-Bhonde <92649509+Akash-R-Bhonde@users.noreply.github.com>
2021-10-17 19:05:54 +0530

    The method can take up to two numbers, separated by commas, and will return their sum.

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -3,7 +3,13 @@ package calculator;
 class StringCalculator {
 
     public int add(String input) {
-        return 0;
+        if(input.isEmpty())
+    	return 0;
+        else if(input.length()==1) return Integer.parseInt(input);
+        else {
+        	String[] s=input.split(",");
+        	return Integer.parseInt(s[0])+Integer.parseInt(s[1]);
+        }
     }
 
 }
